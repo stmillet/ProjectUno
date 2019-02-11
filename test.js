d760269164431cb756fb36726faff3e88b027029
spencermillett <millett.spencer@gmail.com>
2019-02-11 11:40:34 -0700

    StyleSheet

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 
     let username = document.getElementById("username");
     let password = document.getElementById("password");
+
+    function greeting() {
+        username = document.getElementById("username").value;
+        password = document.getElementById("password").value;
+
+        // Code to determine if the password has a symbol, number and capital letter.
+
+        alert("Hello " + username + "! \nWelcome to our website!");
+    }
