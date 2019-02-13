4199af07efbd42f5cbb7ad3276139fede1c2848d
spencermillett <millett.spencer@gmail.com>
2019-02-13 11:40:29 -0700

    Wrote the code to check the password. I wonder how we would store user names and passwords?

--- test.js
+++ test.js
@@ -1,13 +1,72 @@
 
-    let username = document.getElementById("username");
-    let password = document.getElementById("password");
+
 
     function greeting() {
-        username = document.getElementById("username").value;
-        password = document.getElementById("password").value;
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+        let conCaps = hasCapital(password);
+        let conNum = hasNumber(password);
+        let conSym = hasSymbol(password);
+        let output = "";
 
         // Code to determine if the password has a symbol, number and capital letter.
 
+        if (!conCaps) {
 
-        alert("Hello " + username + "! \nWelcome to our website!");
+            output = output + "Your Password needs a capital letter.\n";
+        }
+        if (!conNum) {
+
+            output = output + "Your Password needs a number.\n"
+        }
+        if (!conSym) {
+
+            output = output + "Your Password needs a symbol.\n "
+        }
+        if (conCaps && conNum && conSym) {
+
+            output = "Hello " + username + "! \nWelcome to our website!";
+        }
+
+            alert(output);
+    }
+
+    function hasCapital(password) {
+
+        for (let i = 0; i < password.length; ++i) {
+
+            if (65 <= password.charCodeAt(i) && password.charCodeAt(i) <= 90) {
+
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    function hasNumber(password) {
+
+        for (let i = 0; i < password.length; ++i) {
+
+            if (48 <= password.charCodeAt(i) && password.charCodeAt(i) <= 57) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function hasSymbol(password) {
+
+        for (let i = 0; i < password.length; ++i) {
+
+            if (   (33 <= password.charCodeAt(i) && password.charCodeAt(i) <= 47)
+                || (58 <= password.charCodeAt(i) && password.charCodeAt(i) <= 64)
+                || (91 <= password.charCodeAt(i) && password.charCodeAt(i) <= 96)
+                || (123 <= password.charCodeAt(i) && password.charCodeAt(i) <= 126)) {
+
+                return true;
+            }
+        }
+        return false;
     }
